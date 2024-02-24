899a8237e036827a44bbec1803090b362779ec39
s8900830 <77045479+s8900830@users.noreply.github.com>
2024-02-24 22:36:54 +0800

    修正Dcoker-compose內容、config、env等內容，導入Flask Hostname及Port設定

--- URL_Short/env.py
+++ URL_Short/env.py
@@ -2,14 +2,19 @@ import os
 
 ENV = os.environ.get("Flask_ENV", "development")
 DEBUG = os.environ.get("DEBUG", True)
+FLASK_RUN_HOST = os.environ.get("FLASK_RUN_HOST", "0.0.0.0")
+FLASK_RUN_PORT = os.environ.get("FLASK_RUN_PORT", "5100")
 REDIS_URL = os.environ.get("REDIS_URL", "redis://192.168.1.210:6379/0")
 SQLALCHEMY_DATABASE_URI = os.environ.get(
     "SQLALCHEMY_DATABASE_URI", "mysql://admin:123456789@192.168.1.210:3306/test")
 SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get(
     "SQLALCHEMY_TRACK_MODIFICATIONS", False)
 
+
 # ENV = "development"
 # DEBUG = True
 # REDIS_URL = "redis://192.168.1.210:6379/0"
 # SQLALCHEMY_DATABASE_URI = "mysql://admin:123456789@192.168.1.210:3306/test"
 # SQLALCHEMY_TRACK_MODIFICATIONS = False
+# FLASK_RUN_HOST = 0.0.0.0
+# FLASK_RUN_PORT= 5100
